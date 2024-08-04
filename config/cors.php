f170b6cd9c53348196d79ccd7dedf00dcb492168
mostafafayez <fayezmostafa51@gmail.com>
2024-08-04 18:09:46 +0300

    first commit

--- config/cors.php
+++ config/cors.php
@@ -15,21 +15,15 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+        'paths' => ['api/*'],
+        'allowed_methods' => ['*'],
+        'allowed_origins' => ['http://localhost:3000'],
+        'allowed_origins_patterns' => [],
+        'allowed_headers' => ['*'],
+        'exposed_headers' => [],
+        'max_age' => 0,
+        'supports_credentials' => true,
 
-    'allowed_origins' => ['*'],
-    // 'allowed_origins' => [''],
-
-    'allowed_origins_patterns' => ["*localhost*"],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
 
 ];

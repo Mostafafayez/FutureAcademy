f96a147246b32bf92ff10677bf21f00b3335cead
mostafafayez <fayezmostafa51@gmail.com>
2024-08-04 17:52:06 +0300

    first commit

--- config/cors.php
+++ config/cors.php
@@ -20,9 +20,9 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ["*localhost*"],
 
     'allowed_headers' => ['*'],
 

99d12415589f1f15ac7bca2b6345aea65c0676cb
mostafafayez <fayezmostafa51@gmail.com>
2024-08-04 22:03:07 +0300

    first commit

--- config/cors.php
+++ config/cors.php
@@ -18,12 +18,13 @@ return [
 
         'paths' => ['api/*'],
         'allowed_methods' => ['*'],
-        'allowed_origins' => ['http://localhost:3000'],
+        'allowed_origins' => ['*'],
         'allowed_origins_patterns' => [],
         'allowed_headers' => ['*'],
         'exposed_headers' => [],
         'max_age' => 0,
-        'supports_credentials' => true,
+        'supports_credentials' => false,
+
 
 
 ];

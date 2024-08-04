e11d99237dd5beedf106cd5e4b53391d1f2105af
mostafafayez <fayezmostafa51@gmail.com>
2024-08-05 01:30:40 +0300

    first commit

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -15,6 +15,7 @@ class TrustHosts extends Middleware
     {
         return [
             'localhost:3000',
+            'https://localhost:3000',
             $this->allSubdomainsOfApplicationUrl(),
         ];
     }

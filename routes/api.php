eab806b28826a92efb09c2445e8698644235b001
mostafafayez <fayezmostafa51@gmail.com>
2024-08-04 18:00:12 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ Route::middleware(['auth:sanctum','cors'])->group(function () {
 
 });
 
-Route::middleware('cors')->get('/videos/lesson/{lessonId}', [VideoController::class, 'getByLessonId']);
+Route::get('/videos/lesson/{lessonId}', [VideoController::class, 'getByLessonId']);
 
 
 

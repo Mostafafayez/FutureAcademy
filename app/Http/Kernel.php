9e169d4755d221551e58ece49621f9501c2a63a8
mostafafayez <fayezmostafa51@gmail.com>
2024-08-04 04:04:13 +0300

    first commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
     ];
 
     /**
@@ -72,7 +73,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'course'   =>   \Illuminate\Http\Middleware\HandleCors::class,
+        'cors'   =>   \Illuminate\Http\Middleware\HandleCors::class,
 
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -28,12 +28,13 @@ Route::post('/signup', [AuthController::class, 'signUp']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
-
-Route::group(['middleware' => ['auth:sanctum','course']], function() {
+Route::middleware(['auth:sanctum','cors'])->group(function () {
     Route::get('/teacher/educational-level/{educationalLevelId}', [TeacherController::class, 'getTeachersByEducationalLevel']);
     Route::get('/getsubjects/{educationalLevelId}', [SubjectController::class, 'getByEducationalLevel']);
-    Route::get('/videos/lesson/{lessonId}', [VideoController::class, 'getByLessonId']);
+    Route::get('/videos/lesson/{lessonId}', [VideoController::class, 'getByLessonId']); 
 });
+
+
 
 Route::post('/logout', [AuthController::class, 'logout'])->Middleware('auth:sanctum');
 //subject

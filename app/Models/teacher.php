1480fdbcaa5dfe8cf87c0077e99b2893fb77eeae
mostafafayez <fayezmostafa51@gmail.com>
2024-09-01 12:17:56 +0300

    fix auth

--- app/Models/teacher.php
+++ app/Models/teacher.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-    class teacher extends Authenticatable
+    class teascher extends Authenticatable
     {
         protected $table = 'teachers';
         use HasFactory ;

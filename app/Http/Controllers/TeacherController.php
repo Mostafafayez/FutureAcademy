c0b631711d416f091de6bbbf188b458c6f32eb76
mostafafayez <fayezmostafa51@gmail.com>
2024-09-01 12:22:09 +0300

    fix auth

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Teacher;
+use App\Models\teacher;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\EducationalLevel;

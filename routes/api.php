dbf55853cb64040d5df224f2b07373b2cd2d4619
mostafafayez <fayezmostafa51@gmail.com>
2024-09-28 14:43:59 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -206,6 +206,6 @@ Route::get('/getcode/mohamed_math', [CodeController::class, 'get_mohamed_math'])
 Route::get('/getcode/ashraf', [CodeController::class, 'get_ashraf_codes']);
 
 
-Route::middleware('auth')->post('/update-password', [AuthController::class, 'updatePassword']);
+Route::post('/update-password', [AuthController::class, 'updatePassword']);
 
 

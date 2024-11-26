458574b66111c9b4d21d7b34f9ea27721bac5938
mostafafayez <fayezmostafa51@gmail.com>
2024-11-26 22:45:20 +0200

    first commit

--- app/Models/Code.php
+++ app/Models/Code.php
@@ -29,7 +29,7 @@ class Code extends Model
 
         static::creating(function ($model) {
             // Generate a random string with a specified length (e.g., 8 characters)
-            $model->code = Str::random(8);
+            $model->code = Str::random(12);
         });
     }
     public function user()
